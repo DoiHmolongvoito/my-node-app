78c72d49bb8fb0c101fb912065dceb6d3c48e20e
DoiHmolongvoito <hanhnguyen201104@gmail.com>
2025-06-06 22:37:39 +0700

    Fix PORT binding for Render

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const { Pool } = require('pg');
 const path = require('path');
+require('dotenv').config();  // nhớ thêm dòng này để load .env
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
 
 const app = express();
 const port = process.env.PORT || 3000;
+
 
 // Kết nối tới PostgreSQL thông qua DATABASE_URL
 const pool = new Pool({
@@ -27,6 +30,17 @@ app.get('/api/users', async (req, res) => {
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: 'Lỗi server' });
+  }
+});
+
+// API test kết nối database
+app.get('/db', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Lỗi kết nối database' });
   }
 });
 
